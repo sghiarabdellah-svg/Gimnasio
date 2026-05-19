22aeb15f0397a155e84ae7016cb6eb0dc0be4757
sghiarabdellah-svg <sghiarabdellah@gmail.com>
2026-05-19 17:05:13 +0100

    Repositorios

--- src/main/java/com/example/gimnasio/repositories/ClaseRepository.java
+++ src/main/java/com/example/gimnasio/repositories/ClaseRepository.java
@@ -1,4 +1,14 @@
 package com.example.gimnasio.repositories;
 
-public interface ClaseRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.gimnasio.models.Clase;
+
+public interface ClaseRepository extends JpaRepository<Clase, Long> {
+
+    List<Clase> findByDiaSemana(String diaSemana);
+
+    List<Clase> findByNivel(String nivel);
 }
